bf9dacc8b0bbf5a25db5a436020c7c9ebeb47506
sik2 <bickwang123@gmail.com>
2023-12-04 17:17:20 +0900

    6단계, 구현, 1부, 명령어 파싱

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -3,6 +3,9 @@ package org.example;
 import org.example.System.SystemController;
 import org.example.wiseSaying.WiseSayingController;
 
+import java.util.HashMap;
+import java.util.Map;
+
 
 public class App {
     SystemController systemController;
@@ -26,6 +29,32 @@ public class App {
                 wiseSayingController.create();
             } else if (command.equals("목록")) {
                 wiseSayingController.list();
+            } else if (command.startsWith("삭제")) {
+                //삭제?id=1
+                System.out.println(command);
+//                ["삭제", "id=1"]
+                String[] commandList = command.split("\\?", 2);
+
+                String actionCode = commandList[0]; // 삭제
+
+                String[] paramsList = commandList[1].split("&"); // id=1 & +a
+
+
+                Map<String, String> params = new HashMap<>();
+
+                for (String paramsRow :paramsList) {
+                    String[] paramsStr = paramsRow.split("=", 2);
+
+                    String key = paramsStr[0]; //id
+                    String value = paramsStr[1]; //2
+
+                    params.put(key, value);
+                }
+
+                System.out.println("actionCode :" + actionCode);
+                System.out.println("params :" + params);
+
+//                wiseSayingController.delete();
             }
         }
 
--- src/main/java/org/example/wiseSaying/WiseSayingController.java
+++ src/main/java/org/example/wiseSaying/WiseSayingController.java
@@ -33,4 +33,7 @@ public class WiseSayingController {
         }
     }
 
+    public void delete() {
+
+    }
 }
